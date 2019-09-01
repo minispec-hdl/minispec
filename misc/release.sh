8cb9e6487eb8c415a23c40a0db1622da8b418d86
Daniel Sanchez <sanchez@csail.mit.edu>
2019-09-01 07:29:23 -0400

    [misc/release] Include synth, jupyter, docs, and Vagrantfile

--- misc/release.sh
+++ misc/release.sh
@@ -5,5 +5,6 @@ strip msc
 RELDIR=minispec_r`python misc/gitver.py | cut -d: -f2`
 rm -rf $RELDIR
 mkdir $RELDIR
-cp -r examples tests misc src README.md SConstruct msc $RELDIR/
+cp -r examples tests misc src synth docs jupyter syntax README.md SConstruct Vagrantfile msc $RELDIR/
+rm $RELDIR/synth/full.lib # it's gigantic and not needed
 env GZIP=-9 tar czvf $RELDIR.tar.gz $RELDIR/
